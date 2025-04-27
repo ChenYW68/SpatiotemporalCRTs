3967477001daa9adf203450b9bb5339735e7634a
YW Chen <chenyw20150908@163.com>
2025-04-26 22:15:02 -0400

    Create RDependPackages.R

---
+++ LoadPackages/RDependPackages.R
@@ -0,0 +1,54 @@
+# # 1 loading packages ---------------------------------------
+packages <- c(#"RandomFields",
+              "data.table",
+              "ggplot2",
+              "plyr",
+              "parallel",
+              "sqldf",
+              "latex2exp",
+              "numDeriv",
+              "lubridate",
+              "dplyr",
+              "INLA",
+              "LaplacesDemon",
+              "Hmisc",
+              "MASS",
+              "inlabru",
+              "tidyr",
+              "RColorBrewer",
+              "progress",
+              "fields",
+              "ranger",
+              "MBA",
+              "Rcpp",
+              "writexl",
+              "readxl",
+              "inlabru",
+              "ggmap",
+              "verification",
+              "mapproj",
+              "sp",
+              "mvnfast",
+              "spam",
+              "scoringutils",
+              "rARPACK"
+              )
+# ,'MASS'
+# 2  library
+for(i in 1:length(packages))
+{
+  if(!lapply(packages[i], require,
+             character.only = TRUE)[[1]])
+  {
+    install.packages(packages[i])
+    # library(packages[i])
+    lapply(packages[i], require,
+           character.only = TRUE)
+  }else{lapply(packages[i], require,
+               character.only = TRUE)}
+}
+# x=lapply(packages, require, character.only = TRUE)
+# rm(list=ls())
+rm(i, packages)
+
+
